6e8e958cb2629d467822eb1423103c79b8c8cb1d
Serverfrog <admin@serverfrog>
2024-10-01 11:22:53 +0200

    Static Site Generations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
 						 title="Your current Experience" type="number" />
 		</div>
 		<div class="grid items-center grid-cols-2">
-			<div class="text-right pr-10"><h1>Level {level}</h1></div>
+			<div class="text-right pr-10"><h2>Level {level}</h2></div>
 			<div>
 				<ProgressRadial value={levelPercentage}>{percentageFormated}%</ProgressRadial>
 			</div>
